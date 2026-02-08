559b598ca330f771c78b7b7fb15bdc80712c1c1a
dm18 <dm18@users.noreply.github.com>
2026-02-07 23:09:25 -0800

    add more post options for deviant art

--- commons/src/interfaces/websites/deviant-art/deviant-art.file.options.interface.ts
+++ commons/src/interfaces/websites/deviant-art/deviant-art.file.options.interface.ts
@@ -2,6 +2,7 @@ import { DefaultFileOptions } from '../../submission/default-options.interface';
 
 export interface DeviantArtFileOptions extends DefaultFileOptions {
   disableComments: boolean;
+  AddWatermark: boolean;
   freeDownload: boolean;
   folders: string[];
   isMature: boolean;
@@ -9,6 +10,10 @@ export interface DeviantArtFileOptions extends DefaultFileOptions {
   scraps: boolean;
   noAI: boolean;
   isAIGenerated: boolean;
+  isExclusive: boolean;
+  isPremiumDownload: boolean;
+  isSubscribers: boolean;
+  isPremiumGallery: boolean;
   isCreativeCommons: boolean;
   isCommercialUse: boolean;
   allowModifications: string;
